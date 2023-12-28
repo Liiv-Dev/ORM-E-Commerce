23106ba2768c2e92239fc86e548873a43ea4b34a
Willie Jones <williejones432@yahoo.com>
2023-12-28 11:06:49 -0600

    updated Product Model columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,38 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    //id: {
-    //  type: DataTypes.INTEGER,
-    //  allowNull: false,
-    //  primaryKey: true,
-    //  auto_Increment: true,
-    //},
-    //product_name: {
-    //  type: DataTypes.STRING,
-    //  allowNull: false,
-    //},
-    //price: {
-    //  type: DataTypes.DECIMAL,
-    //  allowNull: false,
-    //  validate: {
-    //    isDecimal:true,
-    //  },
-    //},
-    //stock: {
-    //  type: DataTypes.INTEGER,
-    //  allowNull: false,
-    //  defautValue: 10,
-    //  validate: {
-    //    isNumeric:true,
-    //  },
-    //},
-    //category_id: {
-    //  type: DataTypes.INTEGER,
-    //  references: {
-    //    model: 'category',
-    //    key: 'id',
-    //  },
-    //},
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      auto_Increment: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal:true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defautValue: 10,
+      validate: {
+        isNumeric:true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
